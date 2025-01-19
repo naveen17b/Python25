c15beaac782c7051bd914c722200d869667058b3
Naveen Krishna <bnaveen1794@gmail.com>
2025-01-20 02:15:47 +0530

    sring file nenamed to string and created new folder named programmes and added a sample while loop

---
+++ basics/stings.py
@@ -0,0 +1,21 @@
+s = 'This is a sample python string example. \nThis is second python string to print.'
+
+print(s)
+
+
+text = 'This is a\sample python\\text to print the\name'    #\name -> \n is a new line
+print(text)
+
+#raw string
+
+sample = r'this is sample\text to\print\name'
+
+print(sample)
+
+# concatenation
+
+concatinate = 3 * 'un' + 'ium'
+
+print(  concatinate)
+
+
---
+++ programs/whileloop.py
@@ -0,0 +1,12 @@
+
+#Intro to the while loop in the pythion programming language
+
+#The while loop is used to iterate over a block of code as long as the test expression is true.
+
+#Syntax of a while loop:
+
+a, b = 0, 10
+
+while a <=b:
+    print(a)
+    a += 1
