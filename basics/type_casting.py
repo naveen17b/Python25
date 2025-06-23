c1f5f643da1f2c815efad5d6db11008f0a4b0050
Naveen Krishna <bnaveen1794@gmail.com>
2025-06-23 12:34:53 +0530

    added a new file with typecasting concept

---
+++ basics/type_casting.py
@@ -0,0 +1,12 @@
+# Casting variables
+s = "10"  # Initially a string
+n = int(s)  # Cast string to integer
+cnt = 5
+f = float(cnt)  # Cast integer to float
+age = 25
+s2 = str(age)  # Cast integer to string
+
+# Display results
+print(n)  
+print(f)  
+print(s2)
