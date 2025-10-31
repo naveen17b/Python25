8e82e97b441b6f9b5799f70534da14205d4dd2e0
Naveen Krishna <bnaveen1794@gmail.com>
2025-10-31 22:22:00 +0530

    Pushing the program to Categorise a Person as 'Minor' or 'Adult' based on age input

---
+++ programs/categorise_Person.py
@@ -0,0 +1,13 @@
+
+'''
+Categorise a Person as 'Minor' or 'Adult' based on age input
+'''
+
+age = 20
+
+if age >= 18:
+    print("You are an adult.")
+elif age > 12:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
