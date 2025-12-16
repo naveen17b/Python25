00b4d9442f1f1087e52d07ec518fb7af4fae7e33
Naveen Krishna <bnaveen1794@gmail.com>
2025-12-16 16:47:15 +0530

    fibonacci series program

---
+++ programs/fibonacciSeries.py
@@ -0,0 +1,12 @@
+
+# Fibonacci series up to n use fibo.mo
+def fib(n):
+    """Return Fibonacci series up to n."""
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+fib(25)
